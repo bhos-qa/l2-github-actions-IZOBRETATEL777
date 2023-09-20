5848e58a354eec69929a8003602a18be85f81e0a
Roman Tolstosheyev <rtolstosheyev@intershop.com>
2023-09-20 10:54:27 +0400

    feat: change addition to division

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("1 + 2 = " + add(1, 2));
+        System.out.println("6 / 2 = " + division(6, 2));
     }
 
-    public static int add(int a, int b) {
-        return a + b;
+    public static int division(int a, int b) {
+        return a / b;
     }
 }

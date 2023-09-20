5ca7c965d4fcc8466fd8c83f1ddcc019eebb4b80
Roman Tolstosheyev <rtolstosheyev@intershop.com>
2023-09-20 10:52:59 +0400

    feat: add new functionality

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("1 + 2 = " + add(1, 2));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
